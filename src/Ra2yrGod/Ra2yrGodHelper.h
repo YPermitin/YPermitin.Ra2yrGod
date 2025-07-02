60048265d86f1b6f1176c6d657ce76265129a640
ypermitin <ypermitin@yandex.ru>
2025-07-02 23:58:07 +0500

    Улучшение работы с памятью сторонних процессов
    - Работа с указателями в памяти процесса игры переведена на шаблонные функции.
    - Очистка от дублирующего кода при работе с указателями.

--- src/Ra2yrGod/Ra2yrGodHelper.h
+++ src/Ra2yrGod/Ra2yrGodHelper.h
@@ -9,4 +9,75 @@ namespace Ra2yrGodHelper
     HMODULE getBaseModuleInfo(const HANDLE hProcess, std::string& baseModuleName);
 
     bool isProcessRunning(HANDLE process);
+
+    template<typename T, typename R>
+    R ReadRemotePointer(HANDLE hProcess,
+        T baseAddress,
+        std::vector<int> offsets)
+    {
+        T addr = baseAddress;
+        T nextPtr = 0;
+
+        for (auto off : offsets) {
+            // читаем из адреса (addr + off)
+            if (!ReadProcessMemory(hProcess,
+                reinterpret_cast<LPCVOID>(addr + off),
+                &nextPtr,
+                sizeof(nextPtr),
+                nullptr))
+            {
+                throw std::runtime_error("ReadProcessMemory failed");
+            }
+            addr = nextPtr;
+        }
+
+        return (R)addr; 
+    }
+
+    template<typename T, typename R>
+    void SetRemotePointer(HANDLE hProcess,
+        T baseAddress,
+        std::vector<int> offsets,
+        R newValue)
+    {
+        T addr = baseAddress;
+        T nextPtr = 0;
+
+        int totalOffsets = offsets.size();
+        int currentOffsetNumber = 0;
+
+        for (auto off : offsets) 
+        {
+            currentOffsetNumber++;
+
+            if (totalOffsets == currentOffsetNumber)
+            {
+                // Последнее смещение адреса в памяти используется
+                // для установки конкретного значения.
+                auto newValueAddress = reinterpret_cast<LPVOID>(addr + off);
+
+                if (!WriteProcessMemory(hProcess,
+                    newValueAddress,
+                    &newValue,
+                    sizeof(newValue),
+                    nullptr))
+                {
+                    throw std::runtime_error("WriteProcessMemory failed");
+                }
+            } else
+            {
+                // читаем из адреса (addr + off).
+                // По этому указателю находим следующий адрес в цепочке.
+                if (!ReadProcessMemory(hProcess,
+                    reinterpret_cast<LPCVOID>(addr + off),
+                    &nextPtr,
+                    sizeof(nextPtr),
+                    nullptr))
+                {
+                    throw std::runtime_error("ReadProcessMemory failed");
+                }
+                addr = nextPtr;
+            }            
+        }
+    }
 }
--- src/Ra2yrGod/Ra2yrGodImpl.cpp
+++ src/Ra2yrGod/Ra2yrGodImpl.cpp
@@ -111,49 +111,19 @@ namespace Ra2yrGodImpl
         // 1-2. Используем данные ранее найденного процесса
         ULONG_PTR baseModuleAddress = (ULONG_PTR)processBaseModule;
 
-        // 3. Получаем значение баланса игрока. 
-        // 3.1. Базовый адрес с учетом первого смещения
-        ULONG_PTR basePointerAddressWithOffset = baseModuleAddress + 0x0070F6CC;
-        int basePointer; // HEX: 209EA5E0, INT: 547268064
-        if (ReadProcessMemory(processHandle, (LPVOID)basePointerAddressWithOffset, &basePointer, sizeof(basePointer), 0))
+        // 3. Получаем значение баланса игрока.
+        std::vector<int> offsets
         {
-            // 3.2. Адрес №1
-            ULONG_PTR moneyPointerAddress_1 = (ULONG_PTR)basePointer + 0;
-            int moneyPointer_1; // HEX: 20849450, INT: 545559632
-            if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_1, &moneyPointer_1, sizeof(moneyPointer_1), 0))
-            {
-                // 3.3. Адрес №2
-                ULONG_PTR moneyPointerAddress_2 = (ULONG_PTR)moneyPointer_1 + 0x2C;
-                int moneyPointer_2; // HEX: 202CC4E0, INT: 539804896
-                if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_2, &moneyPointer_2, sizeof(moneyPointer_2), 0))
-                {
-                    // 3.3. Адрес №3
-                    ULONG_PTR moneyPointerAddress_3 = (ULONG_PTR)moneyPointer_2 + 0x258;
-                    int moneyPointer_3; // HEX: 2084AD40, INT: 545566016
-                    if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_3, &moneyPointer_3, sizeof(moneyPointer_3), 0))
-                    {
-                        // 3.3. Адрес №4
-                        ULONG_PTR moneyPointerAddress_4 = (ULONG_PTR)moneyPointer_3 + 0x4C;
-                        int moneyPointer_4; // HEX: 2084A7F0, INT: 545564656
-                        if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_4, &moneyPointer_4, sizeof(moneyPointer_4), 0))
-                        {
-                            // 3.3. Адрес №5
-                            ULONG_PTR moneyPointerAddress_5 = (ULONG_PTR)moneyPointer_4 + 0x2C;
-                            int moneyPointer_5; // HEX: 202CC4E0, INT: 539804896
-                            if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_5, &moneyPointer_5, sizeof(moneyPointer_5), 0))
-                            {
-                                // 3.3. Адрес №6
-                                ULONG_PTR moneyPointerAddress_6 = (ULONG_PTR)moneyPointer_5 + 0x30C;
-                                if (!ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_6, &moneyValue, sizeof(moneyValue), 0))
-                                {
-                                    moneyValue = -1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+            0x0070F6CC,
+            0,
+            0x2C,
+            0x258,
+            0x4C,
+            0x2C,
+            0x30C
+        };
+
+        moneyValue = Ra2yrGodHelper::ReadRemotePointer<int, int>(processHandle, baseModuleAddress, offsets);
 
         return moneyValue;
     }  
@@ -163,46 +133,18 @@ namespace Ra2yrGodImpl
         ULONG_PTR baseModuleAddress = (ULONG_PTR)processBaseModule;
 
         // 3. Получаем значение баланса игрока.
-        // 3.1. Базовый адрес с учетом первого смещения
-        ULONG_PTR basePointerAddressWithOffset = baseModuleAddress + 0x0070F6CC;
-        int basePointer; // HEX: 209EA5E0, INT: 547268064
-        if (ReadProcessMemory(processHandle, (LPVOID)basePointerAddressWithOffset, &basePointer, sizeof(basePointer), 0))
+        std::vector<int> offsets
         {
-            // 3.2. Адрес №1
-            ULONG_PTR moneyPointerAddress_1 = (ULONG_PTR)basePointer + 0;
-            int moneyPointer_1; // HEX: 20849450, INT: 545559632
-            if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_1, &moneyPointer_1, sizeof(moneyPointer_1), 0))
-            {
-                // 3.3. Адрес №2
-                ULONG_PTR moneyPointerAddress_2 = (ULONG_PTR)moneyPointer_1 + 0x2C;
-                int moneyPointer_2; // HEX: 202CC4E0, INT: 539804896
-                if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_2, &moneyPointer_2, sizeof(moneyPointer_2), 0))
-                {
-                    // 3.3. Адрес №3
-                    ULONG_PTR moneyPointerAddress_3 = (ULONG_PTR)moneyPointer_2 + 0x258;
-                    int moneyPointer_3; // HEX: 2084AD40, INT: 545566016
-                    if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_3, &moneyPointer_3, sizeof(moneyPointer_3), 0))
-                    {
-                        // 3.3. Адрес №4
-                        ULONG_PTR moneyPointerAddress_4 = (ULONG_PTR)moneyPointer_3 + 0x4C;
-                        int moneyPointer_4; // HEX: 2084A7F0, INT: 545564656
-                        if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_4, &moneyPointer_4, sizeof(moneyPointer_4), 0))
-                        {
-                            // 3.3. Адрес №5
-                            ULONG_PTR moneyPointerAddress_5 = (ULONG_PTR)moneyPointer_4 + 0x2C;
-                            int moneyPointer_5; // HEX: 202CC4E0, INT: 539804896
-                            if (ReadProcessMemory(processHandle, (LPVOID)moneyPointerAddress_5, &moneyPointer_5, sizeof(moneyPointer_5), 0))
-                            {
-                                // 3.3. Адрес №6
-                                ULONG_PTR moneyPointerAddress_6 = (ULONG_PTR)moneyPointer_5 + 0x30C;
+            0x0070F6CC,
+            0,
+            0x2C,
+            0x258,
+            0x4C,
+            0x2C,
+            0x30C
+        };
 
-                                WriteProcessMemory(processHandle, (LPVOID)moneyPointerAddress_6, &money, sizeof(money), 0);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        Ra2yrGodHelper::SetRemotePointer<int, int>(processHandle, baseModuleAddress, offsets, money);
     }
     void Ra2yrGod::freezePlayerMoney(bool enable, int targetMoney)
     {
