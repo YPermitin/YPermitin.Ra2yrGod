8ad36f9c16a76154439ffb591b240a1f5e98b734
ypermitin <ypermitin@yandex.ru>
2025-07-03 14:51:15 +0500

    Обновлен интерфейс
    - Исправлено название кнопок и всплывающих окон.
    - Открытие дочерних окон теперь выполняется по центру родительского окна.
    - Автоматическое обновление поля с балансом игрока.
    - При активизации элемента с балансом игрока для редактирования перестает выполняться автообновление значения в нем.
    - Добавлен график динамики изменения баланса игрока со временем.
    - Добавлены подсказки при наведении на кнопки формы.
    - Небольшой рефакторинг.
    - Обновлено описание проекта.

--- src/Ra2yrGod/Application.cpp
+++ src/Ra2yrGod/Application.cpp
@@ -1,16 +1,18 @@
 #include "Application.h"
-
 #include "Ra2yrGodImpl.h"
+
+#include <ctime>
 
 #include "imgui.h"
 
 namespace Ra2yrGod
 {
     static Ra2yrGodImpl::Ra2yrGod god;
-
     static bool windowsShow = true;
-
     static int playerMoney = -1;
+    static time_t lastMoneyCheck;
+    static float balanceHistory[60] = {};
+    static int balanceHistoryItem = 0;
 
     void renderUI(HWND hwnd)
     {
@@ -25,11 +27,11 @@ namespace Ra2yrGod
         std::string mainCommandLabel;
         if (processConnected)
         {
-            mainCommandLabel = "Повторный поиск игры";
+            mainCommandLabel = "Переподключиться к процессу игры";
         }
         else
         {
-            mainCommandLabel = "Найти игру";
+            mainCommandLabel = "Подключиться к процессу игры";
         }
 
         if (ImGui::Button(mainCommandLabel.c_str()))
@@ -38,16 +40,28 @@ namespace Ra2yrGod
             processConnected = god.processConnected(true);
 
             playerMoney = god.getPlayerMoney();
+            time(&lastMoneyCheck);
 
-            ImGui::OpenPopup("Поиск игры");
+            ImGui::OpenPopup("Подключение##ProcessConnect");
         }
-        if (ImGui::BeginPopupModal("Поиск игры", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+
+        // Модальное окно открываем по центру родительского
+        ImVec2 parentPos = ImGui::GetWindowPos();
+        ImVec2 parentSize = ImGui::GetWindowSize();
+        ImVec2 modalSize(300, 150);
+        ImVec2 modalPos = ImVec2(
+            parentPos.x + (parentSize.x - modalSize.x) * 0.5f,
+            parentPos.y + (parentSize.y - modalSize.y) * 0.5f
+        );
+        ImGui::SetNextWindowPos(modalPos, ImGuiCond_Appearing);
+        ImGui::SetNextWindowSize(modalSize, ImGuiCond_Appearing);
+        if (ImGui::BeginPopupModal("Подключение##ProcessConnect", NULL, ImGuiWindowFlags_AlwaysAutoResize))
         {
             int processId = god.getProcessId();
             std::string processName = god.getProcessName();
             if (processId > 0)
             {
-                ImGui::Text("Процесс игры успешно найден!\n\n%s (%d)", processName.c_str(), processId);
+                ImGui::Text("Процесс игры успешно подключен!\n\n%s (%d)", processName.c_str(), processId);
             }
             else
             {
@@ -66,53 +80,59 @@ namespace Ra2yrGod
         if (processConnected)
         {
             ImGui::Text("Процесс: %s (%d)", god.getProcessName().c_str(), god.getProcessId());
-
-            if (ImGui::Button("Получить баланс"))
+                        
+            if (ImGui::Button("Обновить##UpdateBalance"))
             {
                 playerMoney = god.getPlayerMoney();
-
-                ImGui::OpenPopup("Получить баланс");
+                time(&lastMoneyCheck);
             }
+            if (ImGui::IsItemHovered())
+                ImGui::SetTooltip("Обновить данные о текущем балансе игрока.");
 
-            if (ImGui::InputInt("Баланс", &playerMoney, 1, 1000))
-            {
-                god.setPlayerMoney(playerMoney);
-            };
-
-            if (ImGui::BeginPopupModal("Получить баланс", NULL, ImGuiWindowFlags_AlwaysAutoResize))
-            {
-                if (playerMoney >= 0)
-                {
-                    ImGui::Text("Текущий баланс игрока: %d", playerMoney);
-                }
-                else
-                {
-                    ImGui::Text("Не удалось определить баланс игрока!");
-                }
-
-                ImGui::Separator();
-
-                if (ImGui::Button("OK", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
-                ImGui::SetItemDefaultFocus();
-                ImGui::SameLine();
-
-                ImGui::EndPopup();
-            }
-
+            ImGui::SameLine();
             std::string frozenBalanceLabel;
             if (god.playerMoneyFrozenState())
             {
-                frozenBalanceLabel = "Разморозить баланс";
+                frozenBalanceLabel = "Разморозить";
             }
             else
             {
-                frozenBalanceLabel = "Заморозить баланс";
+                frozenBalanceLabel = "Заморозить";
             }
+            frozenBalanceLabel += "##FreezeBalance";
             if (ImGui::Button(frozenBalanceLabel.c_str()))
             {
                 bool playerMoneyFrozen = !god.playerMoneyFrozenState();
                 god.freezePlayerMoney(playerMoneyFrozen, playerMoney);
             }
+            if (ImGui::IsItemHovered())
+                ImGui::SetTooltip("\"Заморозить\" текущий баланс игрока.");
+                        
+            if (ImGui::InputInt("##Balance", &playerMoney, 1, 1000))
+            {
+                god.setPlayerMoney(playerMoney);
+            };            
+            if (!ImGui::IsItemActivated() && !ImGui::IsItemFocused())
+            {
+                // Раз в секунду обновляем информацию о текущем балансе,
+                // если элемент не активен в данный момент.
+                time_t currentDateTime;
+                time(&currentDateTime);
+                int lastCheckTimeLeftSec = std::difftime(currentDateTime, lastMoneyCheck);
+                if (lastCheckTimeLeftSec >= 1)
+                {
+                    playerMoney = god.getPlayerMoney();
+                    time(&lastMoneyCheck);
+
+                    balanceHistoryItem++;
+                    if (balanceHistoryItem > 60)
+                        balanceHistoryItem = 1;
+                    balanceHistory[balanceHistoryItem - 1] = playerMoney;                    
+                }
+            }
+            if (ImGui::IsItemHovered())
+                ImGui::SetTooltip("Денежный баланс игрока.");
+            ImGui::PlotLines("##BalanceStatistic", balanceHistory, 60);
         }
 
         ImGui::End();
--- src/Ra2yrGod/Ra2yrGodImpl.cpp
+++ src/Ra2yrGod/Ra2yrGodImpl.cpp
@@ -14,6 +14,7 @@ namespace Ra2yrGodImpl
     {
         resetState();
     }
+    
     bool Ra2yrGod::initProcess() {
         // 0. Перед запуском сбрасываем состояние
         resetState();
